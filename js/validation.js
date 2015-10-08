1a4d34c6387518694d65db4f1d6ae498f6fa651e
sangeetha321 <kathu89@gmail.com>
2015-10-08 14:29:30 +0530

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -52,3 +52,19 @@
 
     
 }
+function logVal()
+{
+ var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+   var email = document.getElementById("email").value; 
+    if (lname == "") {
+       document.getElementById("divid2").style.backgroundColor="#ffffff";
+       document.getElementById("divid2").innerHTML = " <h5><font color='#ff0000' align='center'>Please input  Last Name..!!</font></h5>";
+        return false;
+    } 
+    if (email == "") {
+      document.getElementById("divid2").style.backgroundColor="#ffffff";
+       document.getElementById("divid2").innerHTML = " <h5><font color='#ff0000' align='center'>Please input  Email..!!</font></h5>";
+        return false;
+    } 
+}
